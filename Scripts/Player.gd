5db9cc579a5963e10f51b2b3f70ccf31c88934c6
SnowWhite856 <69056017+SnowWhite856@users.noreply.github.com>
2024-04-05 21:17:38 +0200

    Skybox update

---
+++ Scripts/Player.gd
@@ -0,0 +1,10 @@
+extends RigidBody3D
+
+
+
+func _process(delta : float) -> void:
+	var input := Vector3.ZERO
+	input.x = Input.get_axis("move_left", "move_right")
+	input.z = Input.get_axis("move_forward", "move_back")
+	
+	apply_central_force(twist_pivot.basis * input * 1200.0 * delta)
